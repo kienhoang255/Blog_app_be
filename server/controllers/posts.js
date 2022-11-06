fe0870014a19f2500aee6fbb8fd3974ea683edd0
kienhoang255 <hoangtrungkien255@gmail.com>
2022-11-06 16:28:07 +0700

    blog_app

---
+++ server/controllers/posts.js
@@ -0,0 +1,48 @@
+import { PostModel } from "../models/PostModel.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const createPost = (req, res) => {
+  try {
+    const newPost = req.body;
+
+    const post = new PostModel(newPost);
+    post.save();
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const updatePost = req.body;
+
+    const post = await PostModel.findOneAndUpdate(
+      { _id: updatePost._id },
+      updatePost,
+      { new: true }
+    );
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await PostModel.findOneAndDelete(req.body._id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
